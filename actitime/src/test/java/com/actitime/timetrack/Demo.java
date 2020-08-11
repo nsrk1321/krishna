0e75df91ab06e8eb598f61e5c7ab81ed9ac1b06d
NSRK <NSRK@DESKTOP-4SSU5PL>
2020-08-11 10:14:19 +0530

    commit

---
+++ actitime/src/test/java/com/actitime/timetrack/Demo.java
@@ -0,0 +1,14 @@
+package com.actitime.timetrack;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Demo {
+
+	
+@Test
+	public void run()
+	{
+	Reporter.log("Dhoni is a best captain", true);
+	}
+}
---
+++ actitime/src/test/java/com/actitime/timetrack/Demo2.java
@@ -0,0 +1,14 @@
+package com.actitime.timetrack;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Demo2 {
+
+	
+@Test
+	public void run()
+	{
+	Reporter.log("virat kholi is a best player", true);
+	}
+}
